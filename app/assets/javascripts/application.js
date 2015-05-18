1a2d9e3413cd34e15de6b254231b792d82165bfe
Kurt Guenther <kurtguenther@gmail.com>
2015-05-18 04:34:45 -0400

    timemarkers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,7 +25,7 @@ var performances = [];
 
 
 var MARATHON_START_TIME = 1435348800;
-var MARATHON_END_TIME = 1435622340;
+var MARATHON_END_TIME = 1435549920; //hack, not computed
 
 var WIDTH_PER_MINUTE = 8.0;
 
@@ -37,8 +37,12 @@ function convertStartTimeToOffset(timestamp){
     return (timestamp - MARATHON_START_TIME) / 60.0 * WIDTH_PER_MINUTE;
 }
 
-function convertTimeStampToDate(timestamp){
-    return moment(timestamp*1000).format("h:mm");
+function convertTimeStampToDate(timestamp, include_am_pm) {
+    if (include_am_pm) {
+        return moment(timestamp * 1000).format("h:mm a");
+    } else {
+        return moment(timestamp * 1000).format("h:mm");
+    }
 }
 
 $.ready(new function(){
@@ -59,15 +63,28 @@ $.ready(new function(){
                 console.log("shows hash created");
 
                 //Make the schedule oh so wide
-                $("#schedule").css("width", convertStartTimeToOffset(MARATHON_END_TIME) + "px");
+                $("#schedule").css("width", convertDurationToWidth((MARATHON_END_TIME-MARATHON_START_TIME)/60) + "px");
 
+                // Get some templates ready
                 var venueRowTemplate = Handlebars.compile($("#venue-row-template").html());
                 var locationTemplate = Handlebars.compile($("#location-template").html());
                 var showTemplate = Handlebars.compile($("#show-template").html());
+                var hourTemplate = Handlebars.compile($("#hour-template").html());
 
                 var schedule = $("#schedule")
                 var locationPanel = $(".location-panel");
 
+                //put up the hour markers
+                var hour_container = $("#time-bar");
+                for(var i = MARATHON_START_TIME; i <MARATHON_END_TIME; i+=60*60){
+                    var vals = {hour: convertTimeStampToDate(i, true),
+                        width: convertDurationToWidth(60),
+                        offset: convertStartTimeToOffset(i) + 75
+                    }
+                    hour_container.append(hourTemplate(vals));
+                }
+
+                // We have the put the venues-labels in reverse
                 for(var i = venues.length-1; i >= 0 ; i--){
                     locationPanel.append(locationTemplate(venues[i]));
                 }
@@ -101,12 +118,7 @@ $.ready(new function(){
             }
         });
 
-        ////Set the schedule draggable
-        //$(".schedule").draggable({
-        //    axis: "x",
-        //    containment:[-100000,0,75,0]
-        //});
-
+        //This keeps the fixed panel lined up.
         var foo = $("#location-panel");
         $(window).scroll(function(){
             foo.css("top", -1 * window.scrollY);
